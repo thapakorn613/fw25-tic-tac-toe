dd9a9a76e1e425864d0146e2db0a4543545b42de
thapakorn613 <thapakorn613@gmail.com>
2020-10-31 23:18:53 +0700

    add Draw

--- main.py
+++ main.py
@@ -86,7 +86,11 @@ def player_input(board):
             print("!!! Invalid Input !!!")
             print("---try again---")
 # ------------------------------------------
-
+def findInList(list, platform):
+    for i in range(len(list)):
+        if list[i] == platform:
+            return True
+    return False
 
 def fill(board, r, c):
     if (board[r][c] == "-"):
@@ -98,13 +102,31 @@ def fill(board, r, c):
 
 def check_win(board):
     # N = len(board)
+    n = 3 
     temp = ""
+    # checkDraw = [False,False,False,False]
     new_board = [[x for x in y] for y in board]
     # print("baord: ",board)
+    amountX = 0
+    amountO = 0
+    for i in range(3):
+        for j in range(3):
+            if(new_board[i][j] =="X"):
+                amountX += 1
+            elif(new_board[i][j] =="O"):
+                amountO += 1
+    # ! Check Case 1 all Row 
+    countCheckCase1 = 0
     for i in range(3):
         x = new_board[i][0]
         y = new_board[i][1]
         z = new_board[i][2]
+        tempCheckDraw = []
+        tempCheckDraw.append(x)
+        tempCheckDraw.append(y)
+        tempCheckDraw.append(z)
+        if ( findInList(tempCheckDraw,"X") and findInList(tempCheckDraw,"O") ):
+            countCheckCase1 += 1
         if (x is y is z == "X"):
             print("X win")
             temp = "X"
@@ -113,14 +135,27 @@ def check_win(board):
             print("O win")
             temp = "O"
             break
+        
+            # temp = "D"
+            # break
         else:
             temp = "-"
+    
     if (temp == "O" or temp == "X"):
         return temp
+
+    # ! Check Case 2 all Col
+    countCheckCase2 = 0
     for i in range(3):
         x = new_board[0][i]
         y = new_board[1][i]
         z = new_board[2][i]
+        tempCheckDraw = []
+        tempCheckDraw.append(x)
+        tempCheckDraw.append(y)
+        tempCheckDraw.append(z)
+        if ( findInList(tempCheckDraw,"X") and findInList(tempCheckDraw,"O") ):
+            countCheckCase2 += 1
         if (x is y is z == "X"):
             print("X win")
             temp = "X"
@@ -133,10 +168,40 @@ def check_win(board):
             temp = "-"
     if (temp == "O" or temp == "X"):
         return temp
-
+    
+    # ! Check Case 3
+    countCheckCase3 = 0
     x = new_board[0][0]
     y = new_board[1][1]
     z = new_board[2][2]
+    checkDrawCheck3 = []
+    checkDrawCheck3.append(x)
+    checkDrawCheck3.append(y)
+    checkDrawCheck3.append(z)
+    if ( findInList(checkDrawCheck3,"X") and findInList(checkDrawCheck3,"O") ):
+        countCheckCase3 += 1
+    if (x is y is z == "X"):
+        print("X win")
+        temp = "X"
+    elif (x is y is z == "O"):
+        print("O win")
+        temp = "O"
+    else:
+        temp = "-"
+    if (temp == "O" or temp == "X"):
+        return temp
+    
+    # ! Check Case 4
+    countCheckCase4 = 0
+    x = new_board[0][2]
+    y = new_board[1][1]
+    z = new_board[2][0]
+    checkDrawCheck4 = []
+    checkDrawCheck4.append(x)
+    checkDrawCheck4.append(y)
+    checkDrawCheck4.append(z)
+    if ( findInList(checkDrawCheck4,"X") and findInList(checkDrawCheck4,"O") ):
+        countCheckCase4 += 1
     if (x is y is z == "X"):
         print("X win")
         temp = "X"
@@ -146,25 +211,15 @@ def check_win(board):
     else:
         temp = "-"
     
+    # ? Check End Result
+    # if (amountO + amountX >= n*n):
+    #     return "D"
+    if (countCheckCase1 >= n and countCheckCase2 >= n and countCheckCase3 >=1 and countCheckCase4 >=1 ):
+        return "D"
     if (temp == "O" or temp == "X"):
         return temp
-    x = new_board[0][2]
-    y = new_board[1][1]
-    z = new_board[2][0]
-    if (x is y is z == "X"):
-        print("X win")
-        temp = "X"
-    elif (x is y is z == "O"):
-        print("O win")
-        temp = "O"
-    else:
-        temp = "-"
-
-
-    if (temp == "O" or temp == "X"):
-        return temp
+    
     return "-"
-
 
 
 # ------------------------------------------
--- test.py
+++ test.py
@@ -5,6 +5,11 @@
 
 import random
 
+def find(list, platform):
+    for i in range(len(list)):
+        if list[i] == platform:
+            return True
+    return False
 
 def main():
     # a = ["X", "O", "A"]
@@ -31,23 +36,30 @@ def main():
     #         print("X win")
     #     else:
     #         print("O win")
-    a = [
-        ["O", "O", "-"],
-        ["X", "X", "X"],
-        ["-", "O", "-"]
-    ]
-    n = 3
-    for i in range(n):
-        x = a[i][0]
-        y = a[i][1]
-        z = a[i][2]
-        if (x is y is z == "X"):
-            print("X win")
-        elif (x is y is z == "O"):
-            print("O win")
-        else:
-            print("- win")
+    # a = [
+    #     ["O", "O", "-"],
+    #     ["X", "X", "X"],
+    #     ["-", "O", "-"]
+    # ]
+    # n = 3
+    # for i in range(n):
+    #     x = a[i][0]
+    #     y = a[i][1]
+    #     z = a[i][2]
+    #     if (x is y is z == "X"):
+    #         print("X win")
+    #     elif (x is y is z == "O"):
+    #         print("O win")
+    #     else:
+    #         print("- win")
 
+    streaming = ['netflix', 'hulu', 'disney+', 'appletv+']
+    platform = 'netflix'
+
+    if find(streaming, platform):
+        print("Platform is found")
+    else:
+        print("Platform does not found")
     # for i in range(len(a)-1):
     #     x = a[i]
     #     y = a[i+1]
